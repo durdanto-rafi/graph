80cf1327974a44254950e1b1e32a00b92763ec6b
Golam Mahmud Rafi <rafi.aust@live.com>
2017-07-23 14:44:26 +0900

    Graph displaying perfectly

--- resources/views/graph/index.blade.php
+++ resources/views/graph/index.blade.php
@@ -1,103 +1,94 @@
-@extends('layouts.app')
-@section('title', 'LMS Log Data')
- 
+@extends('layouts.app') 
+@section('title', 'LMS log data graph') 
 @section('content')
 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Graph</h2>
-            </div>
+
+@if ($message = Session::get('success'))
+    <div class="alert alert-success">
+        <p>{{ $message }}</p>
+    </div>
+@endif 
+
+{!! Form::open(array('id'=>'frmGraph')) !!}
+<div class="row">
+    <div class="col-xs-4 col-sm-4 col-md-4">
+        <div class="form-group">
+            <label>Content Number</label> 
+            {!! Form::text('contentNumber', null, array('placeholder' => 'Name','class' => 'form-control', 'id'=>'txtContentNumber', 'onkeypress'=>'return numberValidate(event);')) !!}
         </div>
     </div>
 
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
+    <div class="col-xs-4 col-sm-4 col-md-4">
+        <div class="form-group">
+            <label>From Date</label>
+            <div class="input-group date">
+                <div class="input-group-addon">
+                    <i class="fa fa-calendar"></i>
+                </div>
+                {!! Form::text('dateFrom', null, array('placeholder' => 'Contract Start Date', 'class' => 'form-control pull-right datepicker', 'onkeypress'=>'return false;')) !!}
+            </div>
+            <!-- /.input group -->
         </div>
-    @endif
+    </div>
 
-    {!! Form::open(array('id'=>'frmGraph')) !!}
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Name</strong> 
-                    {!! Form::text('contentNumber', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+    <div class="col-xs-4 col-sm-4 col-md-4">
+        <div class="form-group">
+            <label>To Date</label>
+            <div class="input-group date">
+                <div class="input-group-addon">
+                    <i class="fa fa-calendar"></i>
                 </div>
+                {!! Form::text('dateTo', null, array('placeholder' => 'Contract Period Date', 'class' => 'form-control pull-right datepicker', 'id'=>'txtPeriodDate', 'onkeypress'=>'return false;')) !!}
             </div>
-
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                <div class="form-group">
-                    <label>Contract Start Date</label>
-                    <div class="input-group date">
-                        <div class="input-group-addon">
-                            <i class="fa fa-calendar"></i>
-                        </div>
-                        {!! Form::text('dateFrom', null, array('placeholder' => 'Contract Start Date', 'class' => 'form-control pull-right', 'id'=>'reservation')) !!}
-                    </div>
-                <!-- /.input group -->
-                </div>
-            </div>
-
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                <div class="form-group">
-                    <label>Contract Period Date</label>
-                    <div class="input-group date">
-                        <div class="input-group-addon">
-                            <i class="fa fa-calendar"></i>
-                        </div>
-                        {!! Form::text('contract_period_day', null, array('placeholder' => 'Contract Period Date', 'class' => 'form-control pull-right datepicker', 'id'=>'txtPeriodDate', 'onkeypress'=>'return false;')) !!}
-                    </div>
-                <!-- /.input group -->
-                </div>
-            </div>
-
-           
-
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                {{ Form::submit('Submit', array('class' => 'btn btn-primary')) }}
-            </div>
-
-             <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <label id="lblMessage"/> 
-                </div>
-            </div>
-
-            <div class="col-md-12">
-                <!-- LINE CHART -->
-                <div class="box box-info">
-                    <div class="box-header with-border">
-                    <h3 class="box-title">Line Chart</h3>
-
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                        </button>
-                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                    </div>
-                    </div>
-                    <div class="box-body chart-responsive">
-                    <div class="chart" id="line-chart" style="height: 400px;"></div>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-                <!-- /.box -->
-            </div>
+            <!-- /.input group -->
         </div>
-    {!! Form::close() !!}
+    </div>
 
-    <div class="row">
-        <!-- /.col (LEFT) -->
-        
-        <!-- /.col (RIGHT) -->
-      </div>
+    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+        {{ Form::submit('Submit', array('class' => 'btn btn-primary')) }}
+    </div>
 
-@endsection
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <label id="lblMessage" />
+        </div>
+    </div>
 
-@section('script') @parent
+    <div class="col-md-12">
+        <!-- LINE CHART -->
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h3 id="graphHeader" class="box-title">View Density</h3>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+            </div>
+            <div class="box-body chart-responsive">
+                <div class="chart" id="line-chart" style="height: 400px;"></div>
+            </div>
+            <!-- Loading (remove the following to stop the loading)-->
+            <div class="overlay">
+                <i class="fa fa-refresh fa-spin"></i>
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+</div>
+{!! Form::close() !!}
+
+@endsection 
+
+
+
+@section('script') 
+@parent
+
 <script type="text/javascript">
+    $('.overlay').hide();
 
-    var morrisData = null;
     //Date picker
     $('.datepicker').datepicker({
         autoclose: true,
@@ -105,48 +96,49 @@
     });
 
     //Date range picker with time picker
-    $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'});
+    $('#reservationtime').daterangepicker({
+        timePicker: true,
+        timePickerIncrement: 30,
+        format: 'MM/DD/YYYY h:mm A'
+    });
 
-    window.onload = function() {
-       loadRules();
-    };
-
-    $('#frmGraph').on('submit', function(e) {
+    $('#frmGraph').on('submit', function (e) {
         e.preventDefault();
-        var contentNumber = 5533;
-        var dateFrom = '2016-03-01 0:00:00';
-        var dateTo = '2016-08-31 0:00:00';
-        var token = $("input[name='_token']").val();
+        $('.overlay').show();
+        $("#line-chart").html("");
+        
         $.ajax({
             url: "{{ route('graphData') }}",
             method: 'POST',
-            data: {contentNumber:contentNumber, dateFrom:dateFrom, dateTo:dateTo, _token:token},
-            success: function(data) {
+            data: $(this).serialize(),
+            success: function (data) {
+                $('.overlay').hide();
                 morrisData = data.durationInSecond;
-                console.log(morrisData);
+                graphHeader.innerText = 'View Density for '+ document.getElementById("txtContentNumber").value;;
 
-                // LINE CHART
-                var line = new Morris.Line({
-                    element: 'line-chart',
-                    resize: true,
-                    data: morrisData,
-                    xkey: 'second',
-                    ykeys: ['count'],
-                    labels: ['Item 1'],
-                    lineColors: ['#3c8dbc'],
-                    hideHover: 'auto',
-                    xLabels: "30sec"
-                });
-                
+                line.setData(data.durationInSecond);
             },
-            error: function( data ) {
-                if ( data.status === 422 ) {
+            error: function (data) {
+                if (data.status === 422) {
                     toastr.error('Something went wrong !');
                 }
             }
         });
+
+        // LINE CHART
+        var line = new Morris.Line({
+            element: 'line-chart',
+            resize: true,
+            data: [0,0],
+            xkey: 'second',
+            ykeys: ['count'],
+            labels: ['View count'],
+            lineColors: ['#3c8dbc'],
+            hideHover: 'auto',
+            parseTime: false,
+            pointSize: 0,
+            xLabelFormat: function(x) { return x.label + ' sec' }
+        });
     });
-    
-    
 </script>
 @stop
